b743c91f7a0586f7104ff57780c20aac701dc0aa
Muhammad Furqan Nasir <mfurqannasir24@gmail.com>
2024-02-03 13:03:25 +0500

    update 3

--- frontend/src/components/QrCode.js
+++ frontend/src/components/QrCode.js
@@ -2,7 +2,7 @@ import React from 'react'
 import QRCode from 'react-qr-code';
 
 export default function QrCode() {
-  const url = "https://github.com/MFurqan007";
+  const url = "https://portfolio-mfurqan007.vercel.app/";
 
   return (
     <div className='w-[180px] h-[180px] bg-[#dbdfe1] flex justify-center items-center rounded-[25px]'>
